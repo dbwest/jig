c11cd1fbd5c171be0520877fc3fb678cb409a7e5
David West <david.b.west@gmail.com>
2021-02-07 17:12:10 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export LANG=en_US.UTF-8
 tmux new-session -d
-tmux split-window -h 'watch timew summary' 
-tmux split-window -v 'tasksh' 
+tmux split-window -h 'watch timew summary'
+tmux split-window -v 'tasksh'
 tmux attach
 tmux reflow
